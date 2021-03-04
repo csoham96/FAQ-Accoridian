bdfa701328a726de58281fc6b1635999caa919dd
soham <csoham96@gmail.com>
2021-03-04 11:21:43 +0530

    routing added

--- proj/src/app/app.module.ts
+++ proj/src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FourCardComponent } from './four-card/four-card.component';
-import { PingComingSoonComponent } from './ping-coming-soon/ping-coming-soon.component';
-
+import {PingComingSoonComponent} from './ping-coming-soon/ping-coming-soon.component';
+import { FaqComponent } from './faq/faq.component'
+const appRoutes :Routes=[
+  {path: 'four-card',component: FourCardComponent},
+  {path: 'ping-coming-soon',component:PingComingSoonComponent},
+  {path: 'faq',component:FaqComponent},
+]
 @NgModule({
   declarations: [
     AppComponent,
     FourCardComponent,
-    PingComingSoonComponent
+    PingComingSoonComponent,
+    PingComingSoonComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- proj/src/app/ping-coming-soon/ping-coming-soon.component.ts
+++ proj/src/app/ping-coming-soon/ping-coming-soon.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 @Component({
   selector: 'app-ping-coming-soon',
   templateUrl: './ping-coming-soon.component.html',
   styleUrls: ['./ping-coming-soon.component.css']
 })
 export class PingComingSoonComponent implements OnInit {
-
+  // form: FormGroup=new FormGroup({
+  //   email:new FormControl('',[
+  //     Validators.email
+  // ])
+  // })
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() { }
+  // onSubmit(form: NgForm) 
+  // {
+  //   console.log(form)
+  // }
 }
